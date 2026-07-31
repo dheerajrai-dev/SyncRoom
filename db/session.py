cde4f2ddfa155d482947a3c0c6186deca25951cf
dheerajrai-dev <dheerajrai.devx@gmail.com>
2026-07-31 16:46:47 +0530

    Day 16: async SQLAlchemy engine and session, connected to Postgres

---
+++ db/session.py
@@ -0,0 +1,11 @@
+import os
+from dotenv import load_dotenv
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+
+load_dotenv()
+
+DATABASE_URL = os.environ["DATABASE_URL"]
+
+engine = create_async_engine(DATABASE_URL, echo=True)
+
+AsyncSessionLocal = async_sessionmaker(engine, expire_on_commit=False)
